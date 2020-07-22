db3a293be7c4a5650f2f3278bf26cbfa3aa83ade
Malo10LeGoff <65285198+Malo10LeGoff@users.noreply.github.com>
2020-07-22 20:25:53 +0200

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -9,27 +9,20 @@ import numpy as np
 import random
 import pandas as pd
 
+### We load our scraped dataset and fill the missing values with 1
+
 df = pd.read_csv("reviews.csv", delimiter = '::',engine = 'python', encoding = 'utf-8')
 df2 = df.values
 df2[df2[:,1]=='None',1] = 1
 df2 = df2[df2[:,1] != 'None1']
 
-for i in range(0,len(df2)):
-    print(df2[i])
-
+### We must transform all of our labels into integers for our model
 for i in range(0,len(df2)):
     if type(df2[i,1]) == str:
         df2[i,1] = int(df2[i,1])
         print('String detected')
         print(i)
         
-for i in range(0,len(df2)):
-    print(df2[i,1])
-
-
-print(df2[57,1])
-### Still some missing values, but there are unclear so we just drop them
-
 ### Preprocessing of the data : cleaning the text
 
 import re
@@ -53,14 +46,12 @@ for review in df2:
     cleaned_review = ' '.join(cleaned_review)
     #print("final review : " + str(cleaned_review))
     corpus.append(cleaned_review)
-    
-print("cleaning finished")
 
 
-### Can lead to errors sometimes, here a review "il ne faut pas le rater" so something positive
+### This cleaning can lead to errors sometimes, In our case we have a review "il ne faut pas le rater" so something positive
 ### turned into "faut rater" after stopwords and stemming so the meaning changed
     
-### Creation of the bag of words models because now we have a lot less words
+### Creation of the bag of words models because after the cleaning we have a lot less words
     
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -68,19 +59,12 @@ cv = CountVectorizer(max_features = 1500)  ## only consider the 1500 more freque
 X = cv.fit_transform(corpus).toarray()
 y = df2[:,1]
 y=y.astype('int')
-for i in range(0,len(y)):
-    print(y[i])
-    print('étape' + str(i))
-    if i > 60:
-        break
 
-print(y[57])    
 ### Split between training and testing set
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-    
-    
+       
 ### Model and training
 
 from sklearn.naive_bayes import GaussianNB
